94d247f5fbba5dcf072824f26471ca39a6b8d729
Antoine Fouqué <antoine.fouque@prestataire.renater.fr>
2025-02-04 15:02:03 +0100

    Init gitlab-ci

---
+++ esup-otp-impl/src/test/java/fr/renater/shibboleth/idp/plugin/authn/esup/otp/context/navigate/AttributeContextStringLookupStrategyTest.java
@@ -0,0 +1,4 @@
+package fr.renater.shibboleth.idp.plugin.authn.esup.otp.context.navigate;
+
+public class AttributeContextStringLookupStrategyTest {
+}
---
+++ esup-otp-impl/src/test/java/fr/renater/shibboleth/idp/plugin/authn/esup/otp/context/navigate/UsernameLookupFromSubjectContextTest.java
@@ -0,0 +1,6 @@
+package fr.renater.shibboleth.idp.plugin.authn.esup.otp.context.navigate;
+
+public class UsernameLookupFromSubjectContextTest {
+
+
+}
